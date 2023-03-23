c6079c26a752a7231c7aef9e56f1017d91062f1e
slavytich64 <slavich1209@mail.ru>
2023-03-23 21:47:34 +0700

    implement HW_1

---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,13 @@
+public class VariablesTheme {
+	public static void main(String[] args) {
+
+		byte num_core = 2;
+		short num_freq = 3000;
+		int num_mem = 8;
+		long num_mem_freq = 1600000;
+		float num_graf = 1.1536f;
+		double num_pi = 1.141592653d;
+		char sym = 'R';
+		boolean isTrue = true;
+	}
+}
